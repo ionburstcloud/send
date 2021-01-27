8e868a642c612bd78ceb1650b52a623336cdeb28
timvisee <tim@visee.me>
2021-01-27 13:34:26 +0100

    Fix footer, properly layout no affiliation notice on small screens

--- app/ui/footer.js
+++ app/ui/footer.js
@@ -69,7 +69,11 @@ class Footer extends Component {
       <footer
         class="flex flex-col md:flex-row items-start w-full flex-none self-start p-6 md:p-8 font-medium text-xs text-grey-60 dark:text-grey-40 md:items-center justify-between"
       >
-        <div>${translate('footerText')}</div>
+        <ul
+          class="flex flex-col md:flex-row items-start md:items-center md:justify-start"
+        >
+          <li class="m-2">${translate('footerText')}</li>
+        </ul>
         <ul
           class="flex flex-col md:flex-row items-start md:items-center md:justify-end"
         >
